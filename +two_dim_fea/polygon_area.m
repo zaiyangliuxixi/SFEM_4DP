4f0709531e4e096da59a871cf6a1f4074d6fa962
zaiyangliuxixi <117498706+zaiyangliuxixi@users.noreply.github.com>
2025-01-29 18:13:16 +0900

    Delete +two_dim_fea directory

--- +two_dim_fea/polygon_area.m
+++
@@ -1,7 +0,0 @@
-function area = polygon_area (coordinates)
-    n = size(coordinates,2);
-    x = coordinates(1,:)'; y = coordinates(2,:)';
-    dx = x - [ x(2:end); x(1) ];    % x_{k} - x_{k+1}
-    dy = y + [ y(2:end); y(1) ];    % y_{k} + y_{k+1}
-    area = (1/2)*sum(dx .* dy);
-end
--- +two_dim_fea/polygon_area_gradient.m
+++
@@ -1,6 +0,0 @@
-function [ area_x, area_y ] = polygon_area_gradient (coordinates)
-    n = size(coordinates,2);
-    x = coordinates(1,:)'; y = coordinates(2,:)';
-    area_x = (1/2) * ( - [ y(n-1); y(n); y(1:n-2) ] + [ y(3:n); y(1); y(2) ] );
-    area_y = (1/2) * (   [ x(n-1); x(n); x(1:n-2) ] - [ x(3:n); x(1); x(2) ] );
-end
